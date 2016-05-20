b08ca1528818c7d4d046aaf42abb3ec6265d4358
CAD Client <CAD Client>
2016-05-20 13:45:43 +0900

    added ex30.py

--- ex30/ex30.py
+++ ex30/ex30.py
@@ -1,3 +1,5 @@
+#-*- cording:utf8
+#http://learnpythonthehardway.org/book/ex30.html
 people = 30
 cars = 40
 trucks = 15
@@ -21,4 +23,3 @@ if people > trucks:
     print "Alright, let's just take the trucks."
 else:
     print "Fine, let's stay home then."
-    
